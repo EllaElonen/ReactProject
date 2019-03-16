d79ebd3520be761333f81456212e3e591bed0a20
Ella <ellaelonen@gmail.com>
2019-03-16 12:42:42 +0100

    Complete

--- src/App.js
+++ src/App.js
@@ -1,67 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.jpg';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import logo from "./logo.jpg";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
-      {name: 'Max', age : 28},
-      {name: 'Manu', age: 29},
-      {name: 'Stephanie', age: 26}
+      { name: 'Max', age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 }
     ],
-    otherState: 'some other value'
-  }
+    otherState: "some other value",
+    randomPersons: [
+      { name: "randomPerson1", age: Math.floor(Math.random() * 30) }
+    ]
+  };
 
-  switchNameHandler = (newName) => {
-  //console.log('Was clicked!');
+  switchNameHandler = (event) => {
     this.setState({
       persons: [
-    {name: 'newName', age : 28},
-    {name: 'Manu', age: 29},
-    {name: 'Stephanie', age: 27}
+        { name: "newName", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 }
+      ]
+    });
+  };
+
+  nameChangeHandler = (event) => {
+    this.setState({
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: event.target.value, age: 29 },
+      { name: "Stephanie", age: 26 }
     ]
-  })
-}
+  });
+};
 
   render() {
+    const style ={
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
-
         <h1>Hello, I´m a React App</h1>
-        <img src={logo} className="App-logo" alt="logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
 
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
 
         <h2>This is a register of person objects</h2>
 
-        <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch name</button>
+        <button 
+        style={style}
+        onClick={this.switchNameHandler.bind(this, "Maximilian")}
+        changed={this.nameChangeHandler}>
+          Switch name
+        </button>
 
-        <Person 
-          name={this.state.persons[0].name}  
-          age={this.state.persons[0].age}>
-        </Person>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, "Max")}
+          changed={this.nameChangeHandler}
+        />
 
-        <Person 
-          name={this.state.persons[1].name} 
+        <Person
+          name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Maximilian')}>
-        </Person>
+          changed={this.nameChangeHandler}
+        />
 
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}>
-        </Person>
-
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          changed={this.nameChangeHandler}
+        />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import "./Person.css"
 
 const Person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p>{props.children}</p>
             <p>I´m {props.name} and I´m {props.age} years old.</p>
-            <p>A random Person is {Math.floor(Math.random() * 30)} years old.</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}></input>
         </div>
     )
 };
